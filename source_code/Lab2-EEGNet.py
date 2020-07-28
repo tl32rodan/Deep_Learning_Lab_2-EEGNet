294a117997bf3ac6607dc4f518f98a945ed27cab
tl32rodan <tl32rodan.cs09g@nctu.edu.tw>
2020-07-28 23:36:16 +0800

    Modify the file into proper format

--- source_code/Lab2-EEGNet.py
+++ source_code/Lab2-EEGNet.py
@@ -96,14 +96,6 @@ line_list.append(acc_test_list)
 plt.figure(figsize=[24,18])
 draw_figure(plt,line_list,label_list, loc='best') 
 
-
-# In[16]:
-
-
-for i in {1,3,5}:
-    print('Test Accuracy = ',line_list[i][-1])
-
-
 # In[23]:
 
 
@@ -112,80 +104,4 @@ print('-------------------------')
 print('With ELU        :',line_list[1][-1],'%')
 print('With Leaky ReLU :',line_list[3][-1],'%')
 print('With ReLU       :',line_list[5][-1],'%')
-
-
-# # Experiment on weight decay
-
-# In[24]:
-
-
-# Leaky_relu
-net = EEGNet(act_f='leakyrelu',hyper=6).to(device)
-optimizer = optim.SGD(net.parameters(),lr=1e-2, weight_decay=0,momentum=0.9)
-lr_sch = optim.lr_scheduler.ReduceLROnPlateau(optimizer)
-#optimizer = optim.Adagrad(net.parameters(),lr=1e-4,lr_decay=0.8,weight_decay=5e-3)
-loss_list, acc_train_list, acc_test_list = run(net, train_data,train_label,test_data,test_label,                                               optimizer = optimizer, scheduler=lr_sch, num_epochs = 300, batch_size = 64,                                               print_freq = 500)
-
-
-# In[25]:
-
-
-print('Test Accuracy:')
-print('-------------------------')
-print('With weight-decay    :',line_list[3][-1],'%')
-print('Without weight-decay :',acc_test_list[-1],'%')
-
-
-# # Experiment on optimizer
-
-# In[26]:
-
-
-opt_acc_list = []
-
-
-# In[27]:
-
-
-net = EEGNet(act_f='leakyrelu',hyper=6).to(device)
-optimizer = optim.SGD(net.parameters(),lr=1e-2, weight_decay=5e-2,momentum=0.9)
-lr_sch = optim.lr_scheduler.ReduceLROnPlateau(optimizer)
-loss_list, acc_train_list, acc_test_list = run(net, train_data,train_label,test_data,test_label,                                               optimizer = optimizer, scheduler=lr_sch, num_epochs = 300, batch_size = 64,                                               print_freq = 500)
-opt_acc_list.append(acc_test_list[-1])
-
-
-# In[28]:
-
-
-net = EEGNet(act_f='leakyrelu',hyper=6).to(device)
-optimizer = optim.Adam(net.parameters(),lr=1e-2, weight_decay=5e-2)
-lr_sch = optim.lr_scheduler.ReduceLROnPlateau(optimizer)
-loss_list, acc_train_list, acc_test_list = run(net, train_data,train_label,test_data,test_label,                                               optimizer = optimizer, scheduler=lr_sch, num_epochs = 300, batch_size = 64,                                               print_freq = 500)
-opt_acc_list.append(acc_test_list[-1])
-
-
-# In[29]:
-
-
-net = EEGNet(act_f='leakyrelu',hyper=6).to(device)
-optimizer = optim.Adagrad(net.parameters(),lr=1e-2, weight_decay=5e-2)
-lr_sch = optim.lr_scheduler.ReduceLROnPlateau(optimizer)
-loss_list, acc_train_list, acc_test_list = run(net, train_data,train_label,test_data,test_label,                                               optimizer = optimizer, scheduler=lr_sch, num_epochs = 300, batch_size = 64,                                               print_freq = 500)
-opt_acc_list.append(acc_test_list[-1])
-
-
-# In[31]:
-
-
-print('Test Accuracy:')
-print('-------------------------')
-print('Use SGD     :',opt_acc_list[0],'%')
-print('Use Adam    :',opt_acc_list[1],'%')
-print('Use Adagrad :',opt_acc_list[2],'%')
-
-
-# In[ ]:
-
-
-
 
